534069f91dc3ec70231681685984916e5aa15f04
hemadri-git <bathulahemadri@gmail.com>
2024-12-30 22:25:02 +0530

    code

--- src/com/binarySearch/BSinRange.java
+++ src/com/binarySearch/BSinRange.java
@@ -2,37 +2,47 @@ package com.binarySearch;
 
 public class BSinRange
 {
-    public static void main(String[] args) {
-        int [] arr = {3,4,5,6,7,8,12,13};
-        int target = 7;
-
-        int index = binarySearch(arr, target);
-        if (index != -1){
-            System.out.println("Element found at: " + index);
-        }
-        else {
-            System.out.println("Not Found");
-        }
-    }
-
-    private static int binarySearch(int[] arr, int target) {
-
-        int low = 3;
-        int high = 6;
-
-        while (low<=high){
-            int mid = low +( high - low)/ 2;
-
-            if (target == arr[mid]){
+    public static int bsrange(int[] arr, int key){
+        int start = 3;
+        int end = 6;
+        while (start<=end){
+            int mid = start + (end - start) / 2;
+            if (key == arr[mid]){
                 return mid;
-            } else if (arr[mid] < target) {
-                low = mid + 1;
+            }
+            else if (key > arr[mid]){
+                start = mid + 1;
             }
             else {
-                high = mid - 1;
+                end = mid - 1;
             }
         }
-
         return -1;
+    }
+    private static void sort(int[] arr){
+        int n = arr.length;
+
+        for (int i = 1; i<n; i++){
+            int current = arr[i];
+            int j = i - 1;
+            while (j>=0 && arr[j] > current){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {3,4,5,6,7,8,12,13};
+        sort(nums);
+        int ele = 7;
+
+        int index = bsrange(nums, ele);
+        if (index != -1){
+            System.out.println(ele+ " found at index "+ index);
+        }
+        else {
+            System.out.println("ele not found");
+        }
     }
 }
--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -3,16 +3,55 @@ package com.test;
 import java.util.*;
 
 class Test {
+    public static int binary(int[] arr, int key){
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start<=end){
+            int mid = start + (end - start) / 2;
+
+            if (key == arr[mid]){
+                return mid;
+            } else if (key> arr[mid]) {
+                start = mid + 1;
+
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void sort(int arr[]) {
+
+        int n = arr.length;
+
+        for (int i = 1; i<n; i++){
+            int current = arr[i];
+            int j = i - 1;
+
+            while (j>=0 && arr[j] > current){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+    }
+
 
     public static void main(String[] args)
     {
-        ArrayList<String> list = new ArrayList<>();
-        list.add(null);
-        list.add(0, "A");
-        list.add(null);
-        list.add(2, "B");
-        list.add("20");
-        list.add(1, "C");
-        System.out.println(list);
+        int[] arr = {6,4,3,1,5,2};
+        int ele = 5;
+
+        sort(arr);
+
+        int index = binary(arr, ele);
+        if (index == -1){
+            System.out.println("ele not found");
+        }
+        else {
+            System.out.println(ele + " found in "+ index);
+        }
     }
 }
