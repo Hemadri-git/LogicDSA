13f284a100ebda07335f3d262b2efe8c6276b999
hemadri-git <bathulahemadri@gmail.com>
2025-02-21 22:22:19 +0530

    code

--- src/com/hema/arrays/ElePresentMoreThanOneTime.java
+++ src/com/hema/arrays/ElePresentMoreThanOneTime.java
@@ -1,25 +1,21 @@
-package com.hema.demo;
+package com.hema.arrays;
 
-import java.util.Scanner;
-
-public class Test
+public class ElePresentMoreThanOneTime
 {
-
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 1, 3, 4, 6, 2, 5, 4, 5, 5};
+        int[] arr = {1,2,1,3,4,6,2,5,4,  5};
         int n = arr.length;
-        int count = 0;
-        boolean[] visited = new boolean[n]; // Track visited elements
+        int count=0;
+        boolean[] visited = new boolean[n];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i<n; i++) {
             if (!visited[i]) {
                 int a = i;
                 boolean isDuplicate = false;
-
                 for (int j = i + 1; j < n; j++) {
                     if (arr[a] == arr[j]) {
-                        visited[j] = true; // Mark duplicate as visited
+                        visited[j] = true;
                         isDuplicate = true;
                     }
                 }
@@ -29,7 +25,6 @@ public class Test
                 }
             }
         }
-        System.out.println(count + " elements are present more than one time");
-
+        System.out.println(count + " is appear more than one time");
     }
 }
